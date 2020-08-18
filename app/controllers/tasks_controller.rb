9a975f2bb12f95d13f4af85ebcd3200df0fca529
Mohamed Soliman <mosoliman4@gmail.com>
2020-08-18 15:14:30 +0200

    before refactor

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,33 @@ class TasksController < ApplicationController
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(params[:task].permit(:title, :details))
+
+      redirect_to tasks_path
+  end
+
+  def edit
+   @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task].permit(:title, :details, :completed))
+     redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
 end
